d63ebdea0cc75e3480d20b38a39076567a9a11ff
jlombardia19 <jlombard@emeal.nttdata.com>
2025-06-09 11:17:15 +0200

    working version
    probada solo con facuras FOES

--- codigovb.js
+++ codigovb.js
@@ -5,38 +5,24 @@
 
             const clientId = $application.user.userId;
 
-            // 2.2) Conectar al servidor Socket.IO
             const socket = io('https://servidor-vb-xml.onrender.com');
-            console.log('creada variable SOCKET');
-            // 2.3) Timeout a 5 minutos (300 000 ms)
+
             const timeoutHandle = setTimeout(() => {
               socket.disconnect();
               reject(new Error('Tiempo de espera agotado'));
             }, 5 * 60 * 1000);
-            console.log('TIMEOUTHANDLER CREADO: ',timeoutHandle);
-            // 2.4) Al conectar, enviar registro
             socket.on('connect', function() {
-              console.log('Conectado al servidor socket');
               socket.emit('register', clientId);
             });
 
-            // 2.5) Al llegar el XML‐respuesta
             socket.on('xml-response', function(data) {
               if (data.id === clientId) {
                 clearTimeout(timeoutHandle);
+                const xml = data.xml;
 
-                // Decodificar el XML (si viene Base64/UTF-8)
-                let xml;
-                try {
-                  xml = decodeURIComponent(escape(data.xmlData));
-                } catch (e) {
-                  xml = data.xmlData;
-                }
+                context.$page.variables.xmlData = xml;
 
-                // Guardar en variable de VB
-                context.variables.xmlData = xml;
 
-                // Disparar evento en VB
                 Actions.fireEvent(context, {
                   event: 'xmlRecieved',
                   payload: {
@@ -49,7 +35,6 @@
               }
             });
 
-            // 2.6) Si hay error de conexión
             socket.on('connect_error', function(err) {
               clearTimeout(timeoutHandle);
               console.error('Error de conexión:', err.message);
